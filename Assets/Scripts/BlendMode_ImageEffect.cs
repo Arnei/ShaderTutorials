5771e9f548eae8bbd56ed4361eb93bb4ed5dbedc
Arnei <arnepokemon@yahoo.de>
2019-04-01 16:44:38 +0200

    OldMovieShader

---
+++ Assets/Scripts/BlendMode_ImageEffect.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BlendMode_ImageEffect : MonoBehaviour
+{
+	#region Variables
+	public Shader curShader;
+	public Texture2D blendTexture;
+	public float blendOpacity = 1.0f;
+	private Material curMaterial;
+	#endregion
+
+	#region Properties
+	Material material
+	{
+		get
+		{
+			if(curMaterial == null)
+			{
+				curMaterial = new Material(curShader);
+				curMaterial.hideFlags = HideFlags.HideAndDontSave;
+			}
+			return curMaterial;
+		}
+	}
+	#endregion
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		if(!SystemInfo.supportsImageEffects)
+		{
+			enabled = false;
+			return;
+		}
+
+		if(!curShader && !curShader.isSupported)
+		{
+			enabled = false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		blendOpacity = Mathf.Clamp(blendOpacity, 0.0f, 1.0f);
+	}
+
+	void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture)
+	{
+		if(curShader != null)
+		{
+			material.SetTexture("_BlendTex", blendTexture);
+			material.SetFloat("_Opacity", blendOpacity);
+
+			Graphics.Blit(sourceTexture, destTexture, material);
+		}
+		else
+		{
+			Graphics.Blit(sourceTexture, destTexture);
+		}
+	}
+
+	void OnDisable()
+	{
+		if(curMaterial)
+		{
+			DestroyImmediate(curMaterial);
+		}
+	}
+}
---
+++ Assets/Shaders/ScreenShader/BlendMode_Effect.shader
@@ -0,0 +1,48 @@
+﻿Shader "CookbookShaders/BlendMode_Effect"
+{
+    Properties
+    {
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _BlendTex ("Blend Texture", 2D) = "white" {}
+        _Opacity ("Blend Opacity", Range(0,1)) = 1
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        Pass
+        {
+        	CGPROGRAM
+        	#pragma vertex vert_img
+        	#pragma fragment frag
+        	#pragma fragmentoption ARB_precision_hint_fastest
+        	#include "UnityCG.cginc"
+
+        	uniform sampler2D _MainTex;
+        	uniform sampler2D _BlendTex;
+        	fixed _Opacity;
+
+
+        	fixed4 frag(v2f_img i) : COLOR
+        	{
+        		// Get the colors from the RenderTexture and the UV's
+        		// from the v2f_img struct
+        		fixed4 renderTex = tex2D(_MainTex, i.uv);
+        		fixed4 blendTex = tex2D(_BlendTex, i.uv);
+
+        		// Perform a multiply Blend mode
+        		//fixed4 blendedMultiply = renderTex * blendTex;
+        		//fixed4 blendedMultiply = renderTex + blendTex;
+        		fixed4 blendedMultiply = (1.0 - ((1.0 - renderTex) * (1.0 - blendTex)));
+
+        		// Apply the Luminosity values to our render texture
+        		renderTex = lerp(renderTex, blendedMultiply, _Opacity);
+
+        		return renderTex;
+        	}
+
+        	ENDCG
+        }
+    }
+}
